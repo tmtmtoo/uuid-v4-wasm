cb61ef2b7a212f0cd561c7e25e7f632659e813c2
tmtmtoo <tmtmt2209@gmail.com>
2018-12-22 16:32:51 +0900

    chore(env): support both nodejs and browser

--- build.js
+++ build.js
@@ -15,17 +15,18 @@ const packageJson = require('./package.json')
 const build = packageName =>
     exec(`
         cargo +nightly build --release --target wasm32-unknown-unknown && \
-        mkdir lib && \
-        wasm-bindgen target/wasm32-unknown-unknown/release/${packageName}.wasm --out-dir lib
+        mkdir node browser && \
+        wasm-bindgen --browser target/wasm32-unknown-unknown/release/${packageName}.wasm --out-dir browser && \
+        wasm-bindgen --nodejs target/wasm32-unknown-unknown/release/${packageName}.wasm --out-dir node
     `)
 
-const flowgen = packageName =>
-    exec(`./node_modules/.bin/flowgen ./lib/${packageName}.d.ts -o ./lib/${packageName}.js.flow`)
+const flowgen = (packageName, dir) =>
+    exec(`./node_modules/.bin/flowgen ./${dir}/${packageName}.d.ts -o ./${dir}/${packageName}.js.flow`)
 
-const injectFlowAnnotation = packageName =>
-    readFile(`./lib/${packageName}.js.flow`)
+const injectFlowAnnotation = (packageName, dir) =>
+    readFile(`./${dir}/${packageName}.js.flow`)
         .then(data => `// @flow \n\n${data.toString()}`)
-        .then(data => writeFile(`./lib/${packageName}.js.flow`, data))
+        .then(data => writeFile(`./${dir}/${packageName}.js.flow`, data))
 
 const hyphen2Underscore = str => str.replace(/-/g, '_')
 
@@ -35,6 +36,8 @@ const hyphen2Underscore = str => str.replace(/-/g, '_')
 Promise.resolve(hyphen2Underscore(packageJson.name))
     .then(packageName =>
         build(packageName)
-            .then(() => flowgen(packageName))
-            .then(() => injectFlowAnnotation(packageName))
+            .then(() => flowgen(packageName, 'browser'))
+            .then(() => injectFlowAnnotation(packageName, 'browser'))
+            .then(() => flowgen(packageName, 'node'))
+            .then(() => injectFlowAnnotation(packageName, 'node'))
     )
